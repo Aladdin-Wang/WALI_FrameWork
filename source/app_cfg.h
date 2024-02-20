4403dffffc0b627f2a2c3c979014e7add0e3bc30
KK <53324574+Aladdin-Wang@users.noreply.github.com>
2024-02-20 17:00:58 +0800

    fix app_cfg

--- source/app_cfg.h
+++ source/app_cfg.h
@@ -110,6 +110,24 @@
 #define USER_DATA_SIZE            96
 // </e>
 #define USER_DATA_SIZE_ALIGND     (((USER_DATA_SIZE) + (32) - 1) & ~((32) - 1))
+
+typedef struct {
+    char chProjectName[16];
+    char chHardWareVersion[16];
+    char chSoftBootVersion[16];
+    char chSoftAppVersion[16];
+    char wBoardId;
+    char chPortName[16];
+    int wPortBaudrate;
+    int wRestartTime;
+} msgSig_t;
+typedef struct {
+    union {
+        msgSig_t sig;
+        char B[sizeof(msgSig_t)];
+    } msg_data;
+} user_data_t;
+
 // <<< end of configuration section >>>
 
 #endif
